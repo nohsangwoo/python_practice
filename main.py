7ff5d7b10664e38bdc50738a6136232e81d5b86f
sangwoo noh <nsgr12@gmail.com>
2020-11-04 10:47:41 +0000

    기본 사용법

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world!!")
+x="노상우는"
+y=1989
+z= "년에 태어났다"
+
+# 캐스팅을 해주면 됨
+print(x,str(y),z)
+
+
+# boolean 사용법 : 첫글자가 대문자임
+x=True
+y=False
+print(x,y)
+
+if 2>1:
+  print("hello")
+
+
+if not 1>2:
+  print("작동?")
